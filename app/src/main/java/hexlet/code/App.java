bf0fce85a7b5711b1581621129d3e091d45759d9
artemorlov <amorlov.sev@gmail.com>
2025-09-11 13:45:14 +0300

    edit App.java add connection

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,12 +1,18 @@
 package hexlet.code;
 
 import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class App {
@@ -20,6 +26,13 @@ public class App {
         return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project");
     }
 
+    private static String readResourceFile(String fileName) throws IOException {
+        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
+    }
+
     public static void main(String[] args) throws IOException, SQLException {
         var app = getApp();
 
@@ -31,9 +44,23 @@ public class App {
         var hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl(getDatabaseUrl());
 
+        var dataSource = new HikariDataSource(hikariConfig);
+
+        var sql = readResourceFile("schema.sql");
+        BaseRepository.dataSource = dataSource;
+
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
             config.fileRenderer(new JavalinJte());
+        });
+
+        app.before(ctx -> {
+            ctx.contentType("text/html; charset=utf-8");
         });
 
         app.get("/", ctx -> ctx.result("Hello World"));
