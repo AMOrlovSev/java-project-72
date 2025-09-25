d135f274cb3a680b552d68fe09f81b37267cf68d
artemorlov <amorlov.sev@gmail.com>
2025-09-25 13:21:14 +0300

    schema.sql add url_checks

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,7 +1,18 @@
+DROP TABLE IF EXISTS url_checks;
 DROP TABLE IF EXISTS urls;
 
 CREATE TABLE urls (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP
+);
+
+CREATE TABLE url_checks (
+    id BIGSERIAL PRIMARY KEY,
+    url_id BIGINT REFERENCES urls(id) ON DELETE CASCADE,
+    status_code INTEGER,
+    title VARCHAR(255),
+    h1 VARCHAR(255),
+    description TEXT,
     created_at TIMESTAMP
 );
