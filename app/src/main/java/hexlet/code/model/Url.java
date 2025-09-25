4761a3643a27fcb301cd8156a1a08d1711c0dc84
artemorlov <amorlov.sev@gmail.com>
2025-09-25 13:13:22 +0300

    add UrlCheck

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -2,19 +2,26 @@ package hexlet.code.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
-@ToString
 public class Url {
     private Long id;
     private String name;
     private LocalDateTime createdAt;
+    List<UrlCheck> urlChecks;
 
     public Url(String name) {
         this.name = name;
+        this.urlChecks = new ArrayList<>();
+    }
+
+    public void addChech(UrlCheck urlCheck) {
+        urlChecks.add(urlCheck);
+        urlCheck.setUrlId(id);
     }
 }
