0f0f4a5f9942ea61421a5c5d9e112b7d1c4a4536
artemorlov <amorlov.sev@gmail.com>
2025-10-02 18:38:20 +0300

    TestApp

--- app/src/test/java/hexlet/code/TestApp.java
+++ app/src/test/java/hexlet/code/TestApp.java
@@ -29,6 +29,7 @@ import java.time.LocalDateTime;
 import static hexlet.code.App.createTemplateEngine;
 import static hexlet.code.App.readResourceFile;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @Slf4j
 public class TestApp {
@@ -696,5 +697,65 @@ public class TestApp {
 
         assertThat(NamedRoutes.urlPath("789")).isEqualTo("/urls/789");
         assertThat(NamedRoutes.urlChecksPath("999")).isEqualTo("/urls/999/checks");
+    }
+
+    @Test
+    void testAppGetApp() throws IOException, SQLException {
+        assertDoesNotThrow(() -> {
+            Javalin app = App.getApp();
+            assertThat(app).isNotNull();
+
+            JavalinTest.test(app, (server, client) -> {
+
+                var rootResponse = client.get(NamedRoutes.rootPath());
+                assertThat(rootResponse.code()).isEqualTo(HttpStatus.OK.getCode());
+
+                var urlsResponse = client.get(NamedRoutes.urlsPath());
+                assertThat(urlsResponse.code()).isEqualTo(HttpStatus.OK.getCode());
+
+                assertThat(rootResponse.header("Content-Type")).contains("text/html");
+                assertThat(rootResponse.header("Content-Type")).contains("charset=utf-8");
+            });
+        });
+    }
+
+    @Test
+    void testAppMainMethodIntegration() {
+        assertDoesNotThrow(() -> {
+            Javalin app = App.getApp();
+
+            JavalinTest.test(app, (server, client) -> {
+                assertThat(client.get(NamedRoutes.rootPath()).code()).isEqualTo(HttpStatus.OK.getCode());
+                assertThat(client.get(NamedRoutes.urlsPath()).code()).isEqualTo(HttpStatus.OK.getCode());
+
+                assertThat(client.get("/nonexistent").code()).isEqualTo(HttpStatus.NOT_FOUND.getCode());
+            });
+        });
+    }
+
+    @Test
+    void testAppContentTypeConfiguration() {
+        JavalinTest.test(appTest, (server, client) -> {
+            var url = new Url("https://content-type-test.com");
+            try {
+                UrlRepository.save(url);
+                Long urlId = url.getId();
+
+                var response = client.get(NamedRoutes.rootPath());
+                assertThat(response.header("Content-Type")).isEqualTo("text/html;charset=utf-8");
+
+                var urlsResponse = client.get(NamedRoutes.urlsPath());
+                assertThat(urlsResponse.header("Content-Type")).isEqualTo("text/html;charset=utf-8");
+
+                var urlResponse = client.get(NamedRoutes.urlPath(urlId));
+                assertThat(urlResponse.header("Content-Type")).isEqualTo("text/html;charset=utf-8");
+
+                var notFoundResponse = client.get(NamedRoutes.urlPath(999999L));
+                assertThat(notFoundResponse.header("Content-Type")).isEqualTo("text/plain;charset=utf-8");
+
+            } catch (SQLException e) {
+                throw new RuntimeException("Database error during test", e);
+            }
+        });
     }
 }
